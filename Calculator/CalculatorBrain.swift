c87914ec7d8966b3371ad635248ed5ea127c7d6d
Lyubomir Ganev <luboganev@gmail.com>
2015-02-08 13:37:17 +0100

    Added code for PropertyList for Assignment 3

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -52,6 +52,24 @@ class CalculatorBrain {
         learnOp(Op.NullaryOperation("π", { M_PI }))
     }
     
+//    var program: AnyObject { //PropertyList
+//        get {
+//            return opStack.map { $0.description };
+//        }
+//        set {
+//            if let opSymbols = newValue as? Array<String> {
+//                var newOpStack = [Op]()
+//                for opSymbol in opSymbols {
+//                    if let op = knownOps[opSymbol] {
+//                        newOpStack.append(op)
+//                    } else if let operand = NSNumberFormatter().numberFromString(opSymbol)?.doubleValue {
+//                        newOpStack.append(.Operand(operand))
+//                    }
+//                }
+//            }
+//        }
+//    }
+    
     private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
         if !ops.isEmpty {
             var remainingOps = ops
